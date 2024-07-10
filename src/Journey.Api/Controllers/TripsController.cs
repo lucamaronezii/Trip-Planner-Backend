e13adda9b0302245ef6d2ac160c9882de6fb09d4
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-07-10 13:55:34 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,21 @@
+﻿using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+using Journey.Application.UseCases.Trips.Register;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            var useCase = new RegisterTripUseCase();
+
+            useCase.Execute(request);
+
+            return Created();
+        }
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                throw new ArgumentException("Nome não pode ser vazio.");
+            }
+
+            if (request.StartDate.Date < DateTime.UtcNow.Date)
+            {
+                throw new ArgumentException("Data de início não pode ser antes do momento atual.");
+            }
+
+            if (request.EndDate.Date < request.StartDate.Date)
+            {
+                throw new ArgumentException("Data de fim deve ser durante ou após a data de início.");
+            }
+        }
+    }
+}
